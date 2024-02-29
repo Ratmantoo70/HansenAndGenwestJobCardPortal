b9a3ddc8747fee184b43ebdc94361c45e5176449
Richard <71374981+Ratmantoo70@users.noreply.github.com>
2024-02-29 12:13:01 +0200

    Latest - change to sqlite

--- HansenAndGenwestJobCardPortal/Program.cs
+++ HansenAndGenwestJobCardPortal/Program.cs
@@ -39,9 +39,14 @@ namespace HansenAndGenwestJobCardPortal
                 })
                 .AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(connectionString));
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+            {
+                options.UseSqlite("Data Source = HansenAndGenwest.db");
+            });
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
